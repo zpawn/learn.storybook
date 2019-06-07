89e16c5fd369c6ae41bdb2c93303d3bd61f8c52a
i.makarov <i.makarov@andersenlab.com>
2019-06-07 16:49:39 +0300

    add snapshot test

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import { configure } from "@storybook/react";
+import requireContext from "require-context.macro";
+
 import "../node_modules/papercss/dist/paper.min.css";
 
-const req = require.context("../src", true, /.stories.js$/);
+const req = requireContext("../src", true, /.stories.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
